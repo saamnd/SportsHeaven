fa8d3caf05a7dacb6409c8821af3261313162436
Gabriel <sam.perezf@hotmail.com>
2022-06-04 16:50:56 -0500

    javascript

---
+++ index.js
@@ -0,0 +1,36 @@
+const  express=require('express')
+const PORT = process.env.PORT || 8080
+const bodyParser = require('body-parser')
+const session = require('express-session')
+//const db = require('./dao/models')
+//const usuario = require('./dao/models/usuario')
+const app = express()
+
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended:true
+})) 
+
+app.use(express.static('assets'))
+app.set('view engine','ejs')
+app.use(session({
+    secret : "sam",
+    resave : false,
+    saveUninitialized : false
+}))
+
+
+app.get('/ej1', (req, res) => {
+    const textoRespuesta = "sam"
+    res.send(textoRespuesta)  
+  })
+
+
+
+app.listen(PORT,()=>{
+    console.log(`El servidor se inicio en el puerto: ${PORT}`)
+})
+ 
+ 
